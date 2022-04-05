037aa9451464f2edd11bf170549c319c7645d4fe
enzodragoun <enzo.dragoun@rede.ulbra.br>
2022-04-05 20:13:39 -0300

    ProjetoPW Cont Aula 5

---
+++ PW/PrejetoPW/www/views/templates/footer.php
@@ -0,0 +1,3 @@
+<footer class="jumbotron text-center">
+    <h3>Rodape do nosso site.</h3>
+</footer>
---
+++ PW/PrejetoPW/www/views/templates/header.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS v5.0.2 -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <!-- Bootstrap JavaScript Libraries -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+    </head>
+    <body>  
+
+        <header class="jumbotron">
+            <h1>Cabecalho do Sistema MVC</h1>
+        </header>
+
+        <div class="row">
+            <nav class="navbar bg-light col-md-3">
+                <ul class="navbar-navd">
+                    <li class="nav-item">
+                        <a id="home" class="nav-link" href="?controller=site&action=home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="home" class="nav-link" href="?controller=site&action=about">Sobre</a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="home" class="nav-link" href="?controller=site&action=products">Produtos e Servicos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="home" class="nav-link" href="?controller=site&action=contact">Contato</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+    </body>
+</html>
